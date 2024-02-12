e9728f2dc222af1840d5af8638f866ca5e3d09c7
Yura buryy <buryy137@gmail.com>
2024-02-12 20:53:41 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Collections.Immutable;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return _context.Orders.OrderByDescending(c => c.Price).FirstAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context.Orders.Where(c => c.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser()
+        {
+            return _context.Users.OrderByDescending(c => c.Orders.Count).FirstAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context.Users.Where(c => c.Status == Enums.UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
